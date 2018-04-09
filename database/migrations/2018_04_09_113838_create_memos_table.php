8b68b9d6ce06e9f126445a1f784d52c81ebff3be
Wouter <Wouterholtrop@hotmail.nl>
2018-04-09 13:42:08 +0200

    Migration Memo's added
    Added the migration for memo's foreign key is not added yet nor Recording

---
+++ database/migrations/2018_04_09_113838_create_memos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->increments('memo_id');
+            $table->timestamps();
+			$table->string('Title');
+			$table->string('Text');
+			$table->string('recording');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
