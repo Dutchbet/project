2cedda9641bbaf1200e61a95dcc9ed7aba02116a
ryan koorn <ryan.spottingames@gmail.com>
2018-04-11 13:49:08 +0200

    Login system V0.1.02
    Translated text English to Dutch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Je bent ingelogd!
                 </div>
             </div>
         </div>
