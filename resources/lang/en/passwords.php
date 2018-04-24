c6fc03dab8b25af31f31432e86bbbee61b1375f9
ryan koorn <ryan.spottingames@gmail.com>
2018-04-24 12:13:01 +0200

    V0.1.02 - Login systeem
    Engelse tekst vertaald naar Nederlands

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Wachtwoorden moeten minimaal 6 tekens bevatten en overeenkomen met de bevestiging.',
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben je een wachtwoord reset link gemaild!',
+    'token' => 'Dit wachtwoordherstel-token is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met dit e-mailadres.",
 
 ];
