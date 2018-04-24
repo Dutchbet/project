8163a1e026de75f39e77db872da4c01bdc245c74
Wouter <Wouterholtrop@hotmail.nl>
2018-04-24 13:37:45 +0200

    V.02.04
    - De jamsessie vragen toegevoegd aan Edit
    - Titel's veranderd van de index zodat de vragen er staan

--- resources/views/examples/edit.blade.php
+++ resources/views/examples/edit.blade.php
@@ -3,29 +3,76 @@
     <div class="container"> 
       <form method="post" action="{{action('ExampleController@update', $id)}}">
         <input name="_method" type="hidden" value="PATCH">
-        <div class="row">
-
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <label for="name">title:</label>
-            <input type="text" class="form-control" name="title" value="{{$examples->title}}">
-          </div>
-        </div>
-
-        <div class="row">
+          <div class="row">
           <div class="col-md-4"></div>
             <div class="form-group col-md-4">
-              <label for="body">body:</label>
-              <input type="body" class="form-control" name="body" value="{{$examples->body}}">
+              <label for="vraag1antwoord">vraag 1 antwoord</label>
+              <input type="text" class="form-control" name="vraag1antwoord">
             </div>
           </div>
-        </div>
-        <div class="row">
+          <div class="row">
           <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
+            <div class="form-group col-md-4">
+              <label for="vraag2antwoord">vraag 2 antwoord</label>
+              <input type="text" class="form-control" name="vraag2antwoord">
+            </div>
           </div>
-        </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag3antwoord">vraag 3 antwoord</label>
+              <input type="text" class="form-control" name="vraag3antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag4antwoord">vraag 4 antwoord</label>
+              <input type="text" class="form-control" name="vraag4antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag5antwoord">vraag 5 antwoord</label>
+              <input type="text" class="form-control" name="vraag5antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag6antwoord">vraag 6 antwoord</label>
+              <input type="text" class="form-control" name="vraag6antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag7antwoord">vraag 7 antwoord</label>
+              <input type="text" class="form-control" name="vraag7antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag8antwoord">vraag 8 antwoord</label>
+              <input type="text" class="form-control" name="vraag8antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag9antwoord">vraag 9 antwoord</label>
+              <input type="text" class="form-control" name="vraag9antwoord">
+            </div>
+          </div>
+          <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="vraag10antwoord">vraag 1o antwoord</label>
+              <input type="text" class="form-control" name="vraag10antwoord">
+            </div>
+          </div>
       {{csrf_field()}}
       </form>
     </div>
--- resources/views/examples/index.blade.php
+++ resources/views/examples/index.blade.php
@@ -12,70 +12,70 @@
     <tbody>
       @foreach($examples as $example)
       <tr>
-        <th> Vraag 1 antwoord</th>
+        <th> Wat heb je gezien en gehoord?</th>
       </tr>
       <tr>
         <td>{{$example['vraag1antwoord']}}</td>
       </tr>
       <tr>
-        <th> Vraag 2 antwoord</th>
+        <th> wat heb jij daarbij gedacht en gevoeld ?</th>
       </tr>
       <tr>
         <td>{{$example['vraag2antwoord']}}</td>
       </tr>      
       <tr>
-        <th> Vraag 3 antwoord</th>
+        <th> Wat raakt je in de situatie, welke waarde is voor jou in het geding ?</th>
       </tr>
       <tr>
         <td>{{$example['vraag3antwoord']}}</td>
       </tr>      
       <tr>
-        <th> Vraag 4 antwoord</th>
+        <th> Welke andere waarden zijn er voor jou of voor andere in de situatie in het geding?</th>
       </tr>
       <tr>
         <td>{{$example['vraag4antwoord']}}</td>
       </tr>            
       <tr>
-        <th> Vraag 5 antwoord</th>
+        <th>Wie speelt welke rol en klopt dat volgens jou?</th>
       </tr>
       <tr>
         <td>{{$example['vraag5antwoord']}}</td>
       </tr>      
       <tr>
-        <th> Vraag 6 antwoord</th>
+        <th>Wie heeft welk belang en wlke tegenstellingen zijn daarin?</th>
       </tr>
         <td>{{$example['vraag6antwoord']}}</td>
       </tr>
       <tr>
-        <th> Vraag 7 antwoord</th>
+        <th>Formuleer je gewetensvraag, Begin met: moet..? Of: mag...?</th>
       </tr>
       <tr>
         <td>{{$example['vraag7antwoord']}}</td>
       </tr>
       <tr>
-        <th> Vraag 8 antwoord</th>
+        <th>Zijn er nog meer gewetensvragen te formuleren?</th>
       </tr>
       <tr>
         <td>{{$example['vraag8antwoord']}}</td>
       </tr>
       <tr>
-        <th> Vraag 9 antwoord</th>
+        <th>Welke vragen zijn nog niet beantwoord?</th>
       </tr>
       <tr>
         <td>{{$example['vraag9antwoord']}}</td>
       </tr>
       <tr>
-        <th> Vraag 10 antwoord</th>
+        <th>Welke stappen ga je in welke volgorde zetten?</th>
       </tr>
       <tr>
         <td>{{$example['vraag10antwoord']}}</td>
       </tr>
       <tr>
-        <td>  <a href="{{action('ExampleController@edit', $example['id'])}}" class="btn btn-warning">Edit</a></td>
-          <form action="{{action('ExampleController@destroy', $example['id'])}}" method="post">
+        <td><a href="{{action('ExampleController@edit', $example['id'])}}" class="btn btn-warning">Edit</a></td>
+        <td>  <form action="{{action('ExampleController@destroy', $example['id'])}}" method="post">
             {{csrf_field()}}
             <input name="_method" type="hidden" value="DELETE">
-           <td> <button class="btn btn-danger" type="submit">Delete</button></td>
+            <button class="btn btn-danger" type="submit">Delete</button></td>
           </form>
       </tr>
       @endforeach
