ecc1f5053e2d40b92b95097f4cfe6435c9f2d2c9
ryan koorn <ryan.spottingames@gmail.com>
2018-04-24 12:34:09 +0200

    V0.1.03 - Login systeem
    Hoofdletters weggehaald

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Wachtwoord') }}</div>
+                <div class="card-header">{{ __('Reset wachtwoord') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
